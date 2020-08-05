643600fb053cd817c999abfffa44cfa9cdef979c
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-05 19:58:00 -0400

    Getting started

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,15 +1,35 @@
+# def my_hash(s, size):
+#     bytes_representation = s.encode()
+#     byte_sum = 0
+#     for i in bytes_representation:
+#         byte_sum += i
+#     return byte_sum % size
+
+
+# my_colors = [None] * 5
+# my_colors[my_hash("aqua", len(my_colors))] = "#00FFFF"
+# print(my_colors[my_hash("aqua", len(my_colors))])
+
+
 class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
+    # def __init__(self, key, value):
+    #     self.key = key
+    #     self.value = value
+    #     self.next = None
+
+    # implmentation using lists
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.next = None
 
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
+# min_capacity = 8
 
 
 class HashTable:
@@ -22,7 +42,7 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
-
+        self.capacity = capacity
 
     def get_num_slots(self):
         """
@@ -36,7 +56,6 @@ class HashTable:
         """
         # Your code here
 
-
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
@@ -44,7 +63,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
     def fnv1(self, key):
         """
@@ -55,7 +73,6 @@ class HashTable:
 
         # Your code here
 
-
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
@@ -64,13 +81,12 @@ class HashTable:
         """
         # Your code here
 
-
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -83,7 +99,6 @@ class HashTable:
         """
         # Your code here
 
-
     def delete(self, key):
         """
         Remove the value stored with the given key.
@@ -93,7 +108,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
     def get(self, key):
         """
@@ -105,7 +119,6 @@ class HashTable:
         """
         # Your code here
 
-
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
@@ -114,7 +127,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
 
 if __name__ == "__main__":
